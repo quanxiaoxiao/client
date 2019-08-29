f3a0e60c658ee20dca196f971aed0dda1cd4eb5c
quan <quan_xiaoxiao@qq.com>
2019-08-29 11:29:24 +0800

    add body fetch

--- src/handler/body.js
+++ src/handler/body.js
@@ -1,9 +1,17 @@
 const httpForward = require('../httpForward');
+const fetch = require('../fetch');
 
 const body = handle => async (ctx) => {
   if (typeof handle === 'function') {
     try {
-      const data = await handle(ctx, httpForward);
+      const data = await handle(
+        ctx,
+        httpForward,
+        options => fetch({
+          ...options,
+          socket: ctx.req.socket,
+        }),
+      );
       ctx.body = data;
     } catch (error) {
       console.log(error);
